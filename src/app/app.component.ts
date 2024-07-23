21c8735ede62f9b160c1a876e3c1bce67e53c463
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-07-23 18:14:55 +0200

    feat: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test';
+  step = signal(0);
+  applyForm = new FormGroup({
+    birthday: new FormControl('')
+  })
+
+  nextStep() {
+    this.step.set(this.step() + 1);
+  }
+
+  prevStep() {
+    if (this.step() >= 1) {
+      this.step.set(this.step() - 1);
+    }
+  }
+
+  submitForm() {
+    console.log('Form result: ', this.applyForm.value)
+    console.log(this.step())
+  }
 }
